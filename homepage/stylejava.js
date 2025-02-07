274476933b566d0f3ba8fa38643141c093ea8554
Xime33 <113552320+Xime33@users.noreply.github.com>
2025-02-07 12:55:15 -0600

    Add files via upload

---
+++ homepage/stylejava.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header.jumbotron');
+    header.addEventListener('click', () => {
+        alert('Welcome to My Website!');
+    });
+});
